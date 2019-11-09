077cac1e29a4f13dbbb0882ce1ba71b1f808e229
jragade <jaideepragade@gmail.com>
2019-11-09 06:23:48 +0000

    Redoing Commit

--- calc.py
+++ calc.py
@@ -4,18 +4,3 @@ def add(x,y):
             pass
         def multiply(x,y):
                 pass
-            def divide(x,y):
-                    pass
-                def square(x,y):
-                        pass
-def add(x,y):
-<<<<<<< HEAD
-        return x+y
-=======
-        x+y
->>>>>>> f64d35d179d6250d12e4d218bc14355783e07c0d
-def divide(x,y):
-        return x/y
-    print (x,y)
-
-
